11c9da789d9fa04678d8f4614e6a395f0ceca3f9
Joseph Le <josephle@me.com>
2016-04-28 16:44:58 -0700

    Add company field by adding Profile model

--- users/models.py
+++ users/models.py
@@ -1,5 +1,20 @@
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Profile(models.Model):
+    """
+    Profile model that stores the additional attributes for the user such as the company.
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile", verbose_name="user")
+    company = models.CharField(max_length=255, blank=True)
+
+
+@receiver(post_save, sender=User)
+def handle_user_save(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
--- users/views.py
+++ users/views.py
@@ -10,3 +10,12 @@ class LoginView(account.views.LoginView):
 
 class SignupView(account.views.SignupView):
     form_class = users.forms.SignupForm
+
+    def after_signup(self, form):
+        self.create_profile(form)
+        super(SignupView, self).after_signup(form)
+
+    def create_profile(self, form):
+        profile = self.created_user.profile
+        profile.company = form.cleaned_data["company"]
+        profile.save()
