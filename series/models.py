bfcaf5f35aef2816b1f8c2d2d6a71f298bfea50e
Brian Tan <tanrui.brian@gmail.com>
2016-04-28 18:15:09 -0700

    Added models for videos and series apps

--- series/models.py
+++ series/models.py
@@ -1,5 +1,17 @@
 from __future__ import unicode_literals
 
+from django.contrib.sites.models import Site
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Series(models.Model):
+    site = models.ForeignKey(Site, on_delete=models.CASCADE)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    thumbnail_url = models.CharField(max_length=255)
+
+    def __str__(self):
+        return "%s %s" % (self.title, self.description)
--- videos/models.py
+++ videos/models.py
@@ -1,5 +1,33 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.sites.models import Site
 
-# Create your models here.
+from series.models import Series
+
+
+class Video(models.Model):
+    series = models.ForeignKey(Series, on_delete=models.CASCADE)
+    site = models.ForeignKey(Site, on_delete=models.CASCADE)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='creator')
+
+    ratings = models.ManyToManyField(User, through='Rating', related_name='ratings')
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    url = models.CharField(max_length=255)
+    thumbnail_url = models.CharField(max_length=255)
+
+    def __str__(self):
+        return "%s %s" % (self.title, self.description)
+
+
+class Rating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
+
+    rating = models.IntegerField()
+
+    def __str__(self):
+        return "%s"
