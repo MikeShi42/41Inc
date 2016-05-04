caab6a231ade8b6152b2f44d61af2b4489aff3a5
Joseph Le <josephle@me.com>
2016-05-04 10:45:27 -0700

    Add site one to one field for Info model

--- websites/models.py
+++ websites/models.py
@@ -1,7 +1,13 @@
 from __future__ import unicode_literals
 
+from django.contrib.sites.models import Site
 from django.db import models
 
 
 class Info(models.Model):
+    site = models.OneToOneField(
+        Site,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
     description = models.TextField()
