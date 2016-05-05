bace18c8b5e766cb4a725401e8cecac51aab5f1c
Joseph Le <josephle@me.com>
2016-05-04 19:06:50 -0700

    Fixed source maps and deleted hardcoded scripts and styles templates

--- gulp/tasks/optimizecss.js
+++ gulp/tasks/optimizecss.js
@@ -1,11 +1,14 @@
 const gulp = require('gulp');
 const nano = require('gulp-cssnano');
 const size = require('gulp-size');
+const sourcemaps = require('gulp-sourcemaps');
 
 
 module.exports = (source, options, dist) => {
     return gulp.src(source)
+        .pipe(sourcemaps.init({ loadMaps: true }))
         .pipe(nano(options))
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest(dist))
         .pipe(size());
 };
