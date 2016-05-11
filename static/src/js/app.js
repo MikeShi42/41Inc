b6ffa4187560c4943ac464d0588dc1d627e822bc
Joseph Le <josephle@me.com>
2016-05-11 11:36:58 -0700

    Got playlist ui example to show up

--- static/src/js/app.js
+++ static/src/js/app.js
@@ -1,10 +1,60 @@
 /* global window */
+window.videojs = require('video.js');
+require('videojs-playlist');
+require('videojs-playlist-ui');
 window.jQuery = window.$ = require('jquery');
-
-const $ = window.$;
-
 require('bootstrap-sass');
 
+const $ = window.$;
+const videojs = window.videojs;
+
 $(() => {
-    console.log('document ready');
+    // fire up the plugin
+    const player = videojs('video');
+    player.playlist([{
+        name: 'Disney\'s Oceans',
+        description: 'Explore the depths of our planet\'s oceans. ' +
+        'Experience the stories that connect their world to ours. ' +
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ' +
+        'sed do eiusmod tempor incididunt ut labore et dolore magna ' +
+        'aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco ' +
+        'laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure ' +
+        'dolor in reprehenderit in voluptate velit esse cillum dolore eu ' +
+        'fugiat nulla pariatur. Excepteur sint occaecat cupidatat non ' +
+        'proident, sunt in culpa qui officia deserunt mollit anim id est ' +
+        'laborum.',
+        duration: 45,
+        sources: [
+            {src: 'http://vjs.zencdn.net/v/oceans.mp4', type: 'video/mp4'},
+            {src: 'http://vjs.zencdn.net/v/oceans.webm', type: 'video/webm'}
+        ],
+        // you can use <picture> syntax to display responsive images
+        thumbnail: [
+            {
+                srcset: 'http://lorempixel.com/400/400',
+                type: 'image/jpeg',
+                media: '(min-width: 400px;)'
+            },
+            {
+                srcset: 'http://lorempixel.com/400/400'
+            }
+        ]
+    }, {
+        name: 'Sintel',
+        description: 'The film follows a girl named Sintel who is searching for a baby dragon she calls Scales.',
+        sources: [
+            {src: 'http://media.w3.org/2010/05/sintel/trailer.mp4', type: 'video/mp4'},
+            {src: 'http://media.w3.org/2010/05/sintel/trailer.webm', type: 'video/webm'},
+            {src: 'http://media.w3.org/2010/05/sintel/trailer.ogv', type: 'video/ogg'}
+        ],
+        thumbnail: false
+    },
+        // This is a really underspecified video to demonstrate the
+        // behavior when optional parameters are missing. You'll get prettier
+        // results with more video metadata!
+        {
+            sources: []
+        }]);
+    player.playlistUi();
 });
+
