7073b827d5228115c5cfb7242beeb7e79103e1a5
Joseph Le <josephle@me.com>
2016-05-11 12:32:54 -0700

    Styled video and video playlist

--- static/src/js/app.js
+++ static/src/js/app.js
@@ -13,16 +13,7 @@ $(() => {
     const player = videojs('video');
     player.playlist([{
         name: 'Disney\'s Oceans',
-        description: 'Explore the depths of our planet\'s oceans. ' +
-        'Experience the stories that connect their world to ours. ' +
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ' +
-        'sed do eiusmod tempor incididunt ut labore et dolore magna ' +
-        'aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco ' +
-        'laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure ' +
-        'dolor in reprehenderit in voluptate velit esse cillum dolore eu ' +
-        'fugiat nulla pariatur. Excepteur sint occaecat cupidatat non ' +
-        'proident, sunt in culpa qui officia deserunt mollit anim id est ' +
-        'laborum.',
+        description: 'Explore the depths of our planet\'s oceans. ',
         duration: 45,
         sources: [
             {src: 'http://vjs.zencdn.net/v/oceans.mp4', type: 'video/mp4'},
@@ -32,8 +23,7 @@ $(() => {
         thumbnail: [
             {
                 srcset: 'http://lorempixel.com/400/400',
-                type: 'image/jpeg',
-                media: '(min-width: 400px;)'
+                type: 'image/jpeg'
             },
             {
                 srcset: 'http://lorempixel.com/400/400'
@@ -47,14 +37,12 @@ $(() => {
             {src: 'http://media.w3.org/2010/05/sintel/trailer.webm', type: 'video/webm'},
             {src: 'http://media.w3.org/2010/05/sintel/trailer.ogv', type: 'video/ogg'}
         ],
-        thumbnail: false
-    },
-        // This is a really underspecified video to demonstrate the
-        // behavior when optional parameters are missing. You'll get prettier
-        // results with more video metadata!
-        {
-            sources: []
-        }]);
+        thumbnail: [
+            {
+                srcset: 'http://lorempixel.com/400/400'
+            }
+        ]
+    }]);
     player.playlistUi();
 });
 
