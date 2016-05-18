a60913b66886c1dfdd33f0cebc5ba48569c8256e
Joseph Le <josephle@me.com>
2016-05-18 11:48:55 -0700

    Add new payment fields to website info model

--- websites/models.py
+++ websites/models.py
@@ -11,3 +11,28 @@ class Info(models.Model):
         primary_key=True,
     )
     description = models.TextField()
+    premium_enabled = models.BooleanField(
+        default=False
+    )
+    price_month = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        default=0.00
+    )
+    price_year = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        default=0.00
+    )
+    stripe_public_key = models.CharField(
+        null=True,
+        max_length=32
+    )
+    stripe_secret_key = models.CharField(
+        null=True,
+        max_length=32
+    )
+    stripe_user_id = models.CharField(
+        null=True,
+        max_length=32
+    )
