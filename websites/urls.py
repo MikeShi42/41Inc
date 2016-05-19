d846de4c968f135dd6f4166a8d56cb8f7870911f
Joseph Le <josephle@me.com>
2016-05-18 23:47:20 -0700

    Added subscribe page

--- websites/urls.py
+++ websites/urls.py
@@ -6,4 +6,5 @@ from dashboard.views import WebsiteCreate
 urlpatterns = [
     url(r'^(?P<site_id>[0-9]+)/$', TemplateView.as_view(template_name="websites/homepage.html"), name="site_homepage"),
     url(r'^(?P<site_id>[0-9]+)/videos/(?P<video_id>[0-9]+)$', TemplateView.as_view(template_name="websites/videos/view.html"), name="site_video"),
+    url(r'^(?P<site_id>[0-9]+)/subscribe/$', TemplateView.as_view(template_name="websites/payments/subscribe.html"), name="site_subscribe"),
 ]
