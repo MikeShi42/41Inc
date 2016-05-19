8f09b68db05163d4c64cb9c2912c1057393b22dc
Kyle Smurlo <ksmurlo@ucsd.edu>
2016-05-19 00:25:36 -0700

    Creator site homepage will now display all series in the database (I'll filter it so that it's only relevant series  once I can test it)

--- websites/urls.py
+++ websites/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import include, url
 from django.views.generic import TemplateView
 
 from dashboard.views import WebsiteCreate
+import websites.views
 
 urlpatterns = [
-    url(r'^(?P<site_id>[0-9]+)/$', TemplateView.as_view(template_name="websites/homepage.html"), name="site_homepage"),
+    # url(r'^(?P<site_id>[0-9]+)/$', TemplateView.as_view(template_name="websites/homepage.html"), name="site_homepage"),
+    url(r'^(?P<site_id>[0-9]+)/$', websites.views.site_homepage, name="site_homepage"),
 ]
--- websites/views.py
+++ websites/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from series.models import Series
+# from .models import
 
 # Create your views here.
+
+
+def site_homepage(request, site_id):
+    # series_for_site = Series.objects.filter(site_id = site_id)
+
+    series_for_site = Series.objects.all()
+    context = {'series_for_site': series_for_site}
+    return render(request, 'websites/homepage.html',context)
