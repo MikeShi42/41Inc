b9e29ced1c5f860919aadec1a7f363c7f6ba14ce
Joseph Le <josephle@me.com>
2016-05-21 02:45:37 -0700

    Fixed more stuff

--- websites/urls.py
+++ websites/urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls import include, url
 from django.views.generic import TemplateView
 import websites.views
+import account.urls
 import users.views
 
 urlpatterns = [
-    url(r"^$", TemplateView.as_view(template_name="websites/homepage.html"), name="home"),
+    url(r"^$", websites.views.site_homepage, name="home"),
     url(r"^signup/$", websites.views.WebsiteSignupView.as_view(), name="account_signup"),
     url(r"^login/$", users.views.LoginView.as_view(), name="account_login"),
     # url(r"^account/", include("users.urls")),
     url(r'^videos/(?P<video_id>[0-9]+)$', TemplateView.as_view(template_name="websites/videos/view.html"), name="site_video"),
 ]
+
+urlpatterns += account.urls.urlpatterns
