70bda3d2bbf58696fb0eead4fa00ecfea1c66f05
mdcovarr <mdcovarr@ucsd.edu>
2016-05-27 13:09:51 -0700

    title and description fields added

--- series/forms.py
+++ series/forms.py
@@ -1,5 +1,19 @@
 from django import forms
+from django.contrib.sites.models import Site
+from django.core.exceptions import ValidationError
+from series.models import Series
 
-class SeriesForm(forms.Form):
-        series_name = forms.CharField();
-        description = forms.CharField(widget=forms.Textarea())
+#class SeriesForm(forms.Form):
+#        series_name = forms.CharField();
+#        description = forms.CharField(widget=forms.Textarea())
+class SeriesForm(forms.ModelForm):
+
+    class Meta:
+        model = Series 
+        fields = ['title', 'description']
+
+    def clean_series(self):
+        series = self.cleaned_data['title']
+        if Site.objects.filter(series=series).count() > 0:
+            raise ValidationError('This series title is already used.')
+        return series
