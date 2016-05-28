35902702fcff53700ed60c2e9d67e8c222d83cef
mdcovarr <mdcovarr@ucsd.edu>
2016-05-27 23:02:07 -0700

    saving new Series

--- dashboard/views/series_views.py
+++ dashboard/views/series_views.py
@@ -1,14 +1,31 @@
-from account.mixins import LoginRequiredMixin
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.sites.models import Site
-from django.views.generic import FormView
-
+from django.views.generic import FormView, UpdateView
+from django.core.urlresolvers import reverse_lazy
+from django.shortcuts import redirect 
+from django.views import generic
+from django.views.generic.edit import CreateView
 from series.forms import SeriesForm
 from series.models import Series 
 
 
 class SeriesCreate(LoginRequiredMixin, SuccessMessageMixin, FormView):
+    #login_url = '/account/login/'
+    model = Series
     template_name = 'dashboard/series/createSeries.html'
     form_class = SeriesForm
-    success_message = "%(title) was created successfully"
-    sucess_url = '/dashboard/series/create'
+    success_message = "%(title)s was created successfully"
+    success_url = '/dashboard/series/create'
+
+  
+    def form_valid(self, form):
+        self.create_series(form)
+        return super(SeriesCreate, self).form_valid(form)
+ 
+    def create_series(self, form):
+        series =  Series(title=form.cleaned_data["title"], 
+                description=form.cleaned_data["description"])
+        series.save()
+        return series 
--- series/forms.py
+++ series/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.sites.models import Site
+#from django.contrib.sites.models import Site
 from django.core.exceptions import ValidationError
 from series.models import Series
 
@@ -13,7 +13,7 @@ class SeriesForm(forms.ModelForm):
         fields = ['title', 'description']
 
     def clean_series(self):
-        series = self.cleaned_data['title']
-        if Site.objects.filter(series=series).count() > 0:
+        series_title = self.cleaned_data['title']
+        if Series.objects.filter(title=series_title).count() > 0:
             raise ValidationError('This series title is already used.')
         return series
