d9a97636d99a73a67ab95ed19bbcddf11b9eb096
liawesomesaucer <vincent.oddrepublic@gmail.com>
2016-05-31 17:46:32 -0700

    Delete customize folder

--- websites/models.py
+++ websites/models.py
@@ -13,3 +13,15 @@ class Info(models.Model):
     )
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.TextField()
+
+    # Customization fields
+    main_color = models.TextField()
+    mainbg_color = models.TextField()
+    nav_color = models.TextField()
+    navbg_color = models
+
+    # Logo
+    logo = models.ImageField(upload_to='logos')
+
+    # Pitch information
+    
